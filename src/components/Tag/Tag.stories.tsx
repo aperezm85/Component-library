e026b3e6a48cbfa281fa301ab50866b9105ea192
Alex Perez <aperezm85@gmail.com>
2025-06-24 21:39:37 +0100

    Finished tag component

--- src/components/Tag/Tag.stories.tsx
+++ src/components/Tag/Tag.stories.tsx
@@ -1,8 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { Tag } from "./Tag";
-import { Sizes } from "../../types/Sizes";
-import { Color } from "../../types/Color";
+import { Sizes } from "@/types/Sizes";
+import { Color } from "@/types/Color";
+import { Hierarchy } from "@/types/Hierarchy";
+import { States } from "@/types/States";
 
 const meta = {
   component: Tag,
@@ -12,16 +14,112 @@ const meta = {
     size: {
       control: {
         type: "select",
-        options: Object.values(Sizes),
       },
+      options: Object.values([
+        Sizes.Large,
+        Sizes.Medium,
+        Sizes.Small,
+        Sizes.XSmall,
+      ]),
       description: "The size of the Tag text.",
       defaultValue: Sizes.Medium,
+    },
+    hierarchy: {
+      control: {
+        type: "select",
+      },
+      options: Object.values([Hierarchy.Primary, Hierarchy.Secondary]),
+      description: "The hierarchy of the Tag.",
+      defaultValue: Hierarchy.Primary,
+    },
+    color: {
+      control: {
+        type: "select",
+      },
+      options: Object.values([
+        Color.Gray,
+        Color.Red,
+        Color.Orange,
+        Color.Yellow,
+        Color.Green,
+        Color.Blue,
+        Color.Purple,
+        Color.Magenta,
+      ]),
+      description: "The color of the Tag.",
+      defaultValue: Color.Gray,
+    },
+    state: {
+      control: {
+        type: "select",
+      },
+      options: Object.values([States.Enabled, States.Disabled]),
+      description: "The state of the Tag.",
+      defaultValue: States.Enabled,
+    },
+    isSelected: {
+      control: {
+        type: "boolean",
+      },
+      description: "Whether the Tag is selected.",
+      defaultValue: false,
+    },
+    iconLeading: {
+      control: {
+        type: "boolean",
+      },
+      description: "Whether the Tag has a leading icon.",
+      defaultValue: false,
+    },
+    leadingIcon: {
+      control: {
+        type: "text",
+      },
+      description: "The leading icon of the Tag.",
+      defaultValue: "",
+    },
+    iconTrailing: {
+      control: {
+        type: "boolean",
+      },
+      description: "Whether the Tag has a trailing icon.",
+      defaultValue: false,
+    },
+    trailingIcon: {
+      control: {
+        type: "text",
+      },
+      description: "The trailing icon of the Tag.",
+      defaultValue: "",
+    },
+    text: {
+      control: {
+        type: "text",
+      },
+      description: "The text of the Tag.",
+      defaultValue: "Label",
     },
   },
 } satisfies Meta<typeof Tag>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    size: Sizes.Medium,
+    hierarchy: Hierarchy.Primary,
+    color: Color.Gray,
+    state: States.Enabled,
+    isSelected: false,
+    text: "Label",
+    iconLeading: true,
+    leadingIcon: "heart-solid",
+    iconTrailing: true,
+    trailingIcon: "heart-solid",
+  },
+  render: (args) => <Tag {...args} />,
+};
 
 export const AllTags: Story = {
   args: {
@@ -31,7 +129,35 @@ export const AllTags: Story = {
   render: (args) => (
     <div className="flex flex-row gap-2">
       {Object.values(Color).map((color) => (
-        <Tag key={color} {...args} color={color} />
+        <div key={color} className="flex flex-col gap-2">
+          <Tag {...args} color={color} hierarchy={Hierarchy.Primary} />
+          <Tag
+            {...args}
+            color={color}
+            hierarchy={Hierarchy.Primary}
+            isSelected
+          />
+          <Tag
+            {...args}
+            color={color}
+            hierarchy={Hierarchy.Primary}
+            state={States.Disabled}
+          />
+          <Tag {...args} color={color} hierarchy={Hierarchy.Secondary} />
+
+          <Tag
+            {...args}
+            color={color}
+            hierarchy={Hierarchy.Secondary}
+            isSelected
+          />
+          <Tag
+            {...args}
+            color={color}
+            hierarchy={Hierarchy.Secondary}
+            state={States.Disabled}
+          />
+        </div>
       ))}
     </div>
   ),
--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,11 +1,11 @@
-import { Sizes } from "../../types/Sizes";
-import { Hierarchy } from "../../types/Hierarchy";
-import { Color } from "../../types/Color";
-import { States } from "../../types/States";
+import { Sizes } from "@/types/Sizes";
+import { Hierarchy } from "@/types/Hierarchy";
+import { Color } from "@/types/Color";
+import { States } from "@/types/States";
 import clsx from "clsx";
 import { Label } from "../Label/Label";
 
-export interface TagProps {
+export interface TagProps extends React.HTMLAttributes<HTMLDivElement> {
   size?:
     | typeof Sizes.Large
     | typeof Sizes.Medium
@@ -46,61 +46,161 @@ export const Tag = ({
 
   let colors = "";
   let sizes = "";
+  let outlineColor = "";
+  let iconFontSize = "";
   switch (color) {
-    case Color.Gray:
-      colors = "bg-gray-600 outline-gray-700 text-gray-50";
-      break;
     case Color.Red:
-      colors = "bg-red-600 outline-red-700 text-red-50";
+      if (state === States.Disabled) {
+        colors = "bg-red-50 text-red-300";
+        outlineColor = "outline-red-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-red-600 text-red-50";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-red-700";
+      } else {
+        colors = "bg-red-50 text-red-700";
+        outlineColor = isSelected ? "outline-red-600" : "outline-gray-100";
+      }
       break;
     case Color.Orange:
-      colors = "bg-orange-600 outline-orange-700 text-orange-50";
+      if (state === States.Disabled) {
+        colors = "bg-orange-50 text-orange-300";
+        outlineColor = "outline-orange-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-orange-600 text-orange-50";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-orange-700";
+      } else {
+        colors = "bg-orange-50 text-orange-700";
+        outlineColor = isSelected ? "outline-orange-600" : "outline-orange-100";
+      }
       break;
     case Color.Yellow:
-      colors = "bg-yellow-300 outline-yellow-400 text-yellow-900";
+      if (state === States.Disabled) {
+        colors = "bg-yellow-50 text-yellow-300";
+        outlineColor = "outline-yellow-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-yellow-300 text-yellow-900";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-yellow-400";
+      } else {
+        colors = "bg-yellow-50 text-yellow-700";
+        outlineColor = isSelected ? "outline-yellow-600" : "outline-yellow-100";
+      }
       break;
     case Color.Green:
-      colors = "bg-green-600 outline-green-700 text-green-50";
+      if (state === States.Disabled) {
+        colors = "bg-green-50 text-green-300";
+        outlineColor = "outline-green-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-green-600 text-green-50";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-green-700";
+      } else {
+        colors = "bg-green-50  text-green-700";
+        outlineColor = isSelected ? "outline-green-600" : "outline-green-100";
+      }
       break;
     case Color.Blue:
-      colors = "bg-blue-600 outline-blue-700 text-blue-50";
+      if (state === States.Disabled) {
+        colors = "bg-blue-50 text-blue-300";
+        outlineColor = "outline-blue-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-blue-600  text-blue-50";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-blue-700";
+      } else {
+        colors = "bg-blue-50 text-blue-700";
+        outlineColor = isSelected ? "outline-blue-600" : "outline-blue-100";
+      }
       break;
     case Color.Purple:
-      colors = "bg-purple-600 outline-purple-700 text-purple-50";
+      if (state === States.Disabled) {
+        colors = "bg-purple-50 text-purple-300";
+        outlineColor = "outline-purple-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-purple-600  text-purple-50";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-purple-700";
+      } else {
+        colors = "bg-purple-50 outline-purple-100 text-purple-700";
+        outlineColor = isSelected ? "outline-purple-600" : "outline-purple-100";
+      }
       break;
     case Color.Magenta:
-      colors = "bg-magenta-600 outline-magenta-700 text-magenta-50";
+      if (state === States.Disabled) {
+        colors = "bg-magenta-50 text-magenta-300";
+        outlineColor = "outline-magenta-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-magenta-600 text-magenta-50";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-magenta-700";
+      } else {
+        colors = "bg-magenta-50 text-magenta-700";
+        outlineColor = isSelected
+          ? "outline-magenta-600"
+          : "outline-magenta-100";
+      }
       break;
     default:
-      colors = "bg-gray-600 outline-gray-700 text-gray-50";
+      if (state === States.Disabled) {
+        colors = "bg-gray-50 text-gray-300";
+        outlineColor = "outline-gray-50";
+        break;
+      }
+      if (hierarchy === Hierarchy.Primary) {
+        colors = "bg-gray-600 text-gray-50";
+        outlineColor = isSelected ? "outline-primaryA" : "outline-gray-700";
+      } else {
+        colors = "bg-gray-50 text-gray-700";
+        outlineColor = isSelected ? "outline-gray-600" : "outline-gray-100";
+      }
   }
 
   switch (size) {
     case Sizes.Large:
       sizes = "h-10 px-2.5";
+      iconFontSize = "text-lg";
       break;
     case Sizes.Medium:
       sizes = "h-8 px-2";
+      iconFontSize = "text-base";
       break;
     case Sizes.Small:
       sizes = "h-6 px-1.5";
+      iconFontSize = "text-sm";
       break;
     default:
       sizes = "h-5 px-1";
+      iconFontSize = "text-xs";
   }
-
   return (
     <div
       className={clsx(
-        "flex items-center justify-center flex-row gap-1",
+        "inline-flex items-center justify-center flex-row gap-1 w-auto",
         "rounded-lg",
-        "outline-2 outline-solid",
-        offset,
         colors,
+        !isSelected && "outline-2 outline-solid",
+        isSelected && "outline-3 outline-solid",
+        outlineColor,
+        offset,
         sizes
       )}
     >
+      {iconLeading && leadingIcon && (
+        <i className={clsx(iconFontSize, `iconoir-${leadingIcon}`)}></i>
+      )}
       {text && <Label size={size} text={text} />}
+      {iconTrailing && trailingIcon && (
+        <i className={clsx(iconFontSize, `iconoir-${trailingIcon}`)}></i>
+      )}
     </div>
   );
 };
